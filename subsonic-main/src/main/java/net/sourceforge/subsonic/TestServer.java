d3fe4654e7d93fd9c6e7fcf0d5a69b214cdf5c16
biconou <rcocula@gmail.com>
2014-03-11 18:57:25 +0100

    Very tiny jetty server for debug purpose.

---
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/TestServer.java
@@ -0,0 +1,21 @@
+package net.sourceforge.subsonic;
+
+ 
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+ 
+public class TestServer {
+ 
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        String baseDir = TestServer.class.getResource("/" + TestServer.class.getName().replaceAll("\\.", "/") + ".class").getFile();
+        baseDir = baseDir.substring(0,baseDir.indexOf("/target"));
+        webapp.setWar(baseDir + "/src/main/webapp");
+        server.setHandler(webapp);
+        server.start();
+        server.join();
+    }
+}
