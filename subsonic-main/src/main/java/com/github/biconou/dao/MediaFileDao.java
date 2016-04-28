1eed62912c94b121b10acae48389db5caa6b68fc
rcocula <remi.cocula@soprasteria.com>
2016-04-28 18:39:57 +0200

    .

--- subsonic-main/src/main/java/com/github/biconou/dao/MediaFileDao.java
+++ subsonic-main/src/main/java/com/github/biconou/dao/MediaFileDao.java
@@ -1,19 +1,15 @@
 package com.github.biconou.dao;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.sourceforge.subsonic.domain.MediaFile;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.bootstrap.Elasticsearch;
-import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.action.search.SearchResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import net.sourceforge.subsonic.domain.MediaFile;
 
 /**
  * Created by remi on 26/04/2016.
  */
 public class MediaFileDao extends net.sourceforge.subsonic.dao.MediaFileDao {
-
-
 
 
     private ElasticSearchClient elasticSearchClient = null;
@@ -23,14 +19,45 @@ public class MediaFileDao extends net.sourceforge.subsonic.dao.MediaFileDao {
 
         if (file.getMediaType().equals(MediaFile.MediaType.MUSIC) || file.getMediaType().equals(MediaFile.MediaType.ALBUM)) {
 
-            try {
+         /* SearchResponse alreadyIndexedMediaFileResponse = getElasticSearchClient().getClient().prepareSearch(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
+            .setQuery(QueryBuilders.idsQuery().ids("" + file.getId()))
+            .execute().actionGet();
+*/
+
+        /* SearchResponse alreadyIndexedMediaFileResponse = getElasticSearchClient().getClient().prepareSearch(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
+            .setQuery(QueryBuilders.constantScoreQuery(QueryBuilders.termQuery("path", file.getPath())))
+           .setPostFilter()
+            .execute().actionGet();
+            */
+
+
+          String jsonSearch = "" +
+            "{" +
+            "    \"constant_score\" : {" +
+            "        \"filter\" : {" +
+            "            \"term\" : {" +
+            "                \"path\" : \""+file.getPath()+"\"" +
+            "            }" +
+            "        }" +
+            "    }" +
+            "}";
+
+
+          SearchResponse alreadyIndexedMediaFileResponse = getElasticSearchClient().getClient().prepareSearch(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
+            .setQuery(jsonSearch).execute().actionGet();
+
+          alreadyIndexedMediaFileResponse.getHits().getTotalHits();
+
+
+          try {
+              //TODO traiter la mise à jour
                 String json = getElasticSearchClient().getMapper().writeValueAsString(file);
-                IndexResponse response = getElasticSearchClient().getClient().prepareIndex(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME, file.getMediaType().toString())
+                IndexResponse response = getElasticSearchClient().getClient().prepareIndex(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME, file.getMediaType().toString(), "" + file.getId())
                         .setSource(json)
                         .get();
 
             } catch (JsonProcessingException e) {
-                e.printStackTrace();
+                throw new RuntimeException("Error trying indexing mediaFile "+e);
             }
 
         } else {
