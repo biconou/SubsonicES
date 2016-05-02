98a071402249a5a14680b0ca53260964a10cc16f
biconou <rcocula@gmail.com>
2016-05-03 00:05:03 +0200

    Unit test for MediaScannerService

--- subsonic-main/src/main/java/com/github/biconou/dao/ElasticSearchClient.java
+++ subsonic-main/src/main/java/com/github/biconou/dao/ElasticSearchClient.java
@@ -1,6 +1,7 @@
 package com.github.biconou.dao;
 
 import java.net.InetAddress;
+
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -12,44 +13,57 @@ import net.sourceforge.subsonic.domain.MediaFile;
  */
 public class ElasticSearchClient {
 
-    public static final String SUBSONIC_MEDIA_INDEX_NAME = "subsonic_media";
+  public static final String SUBSONIC_MEDIA_INDEX_NAME = "subsonic_media";
 
-    public static final String ALBUM_TYPE = MediaFile.MediaType.ALBUM.toString();
-    public static final String MUSIC_TYPE = MediaFile.MediaType.MUSIC.toString();
+  public static final String ALBUM_TYPE = MediaFile.MediaType.ALBUM.toString();
+  public static final String MUSIC_TYPE = MediaFile.MediaType.MUSIC.toString();
 
-    private Client elasticSearchClient = null;
-    private ObjectMapper mapper = new ObjectMapper();
+  private Client elasticSearchClient = null;
+  private ObjectMapper mapper = new ObjectMapper();
 
-    public Client getClient() {
+  private Client obtainESClient() {
+    return TransportClient.builder().build()
+            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLoopbackAddress(), 9300));
+  }
+
+  public void deleteIndex() {
+    Client client = obtainESClient();
+    boolean indexExists = client.admin().indices().prepareExists(SUBSONIC_MEDIA_INDEX_NAME).execute().actionGet().isExists();
+    if (indexExists) {
+      client.admin().indices().prepareDelete(SUBSONIC_MEDIA_INDEX_NAME).execute().actionGet();
+    }
+    elasticSearchClient = null;
+  }
+
+  public Client getClient() {
+    if (elasticSearchClient == null) {
+      synchronized (this) {
         if (elasticSearchClient == null) {
-            synchronized (this) {
-                if (elasticSearchClient == null) {
-                    elasticSearchClient = TransportClient.builder().build()
-                            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLoopbackAddress(), 9300));
+          elasticSearchClient = obtainESClient();
 
-                    boolean indexExists = elasticSearchClient.admin().indices().prepareExists(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
-                            .execute().actionGet().isExists();
-                    if (!indexExists) {
-                        elasticSearchClient.admin().indices()
-                                .prepareCreate(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
-                                .addMapping("DIRECTORY",
-                                        "path","type=string,index=not_analyzed",
-                                        "created","type=date",
-                                        "changed","type=date",
-                                        "childrenLastUpdated","type=date",
-                                        "lastPlayed","type=date",
-                                        "lastScanned","type=date",
-                                        "starredDate","type=date")
-                                .execute().actionGet();
-                    }
+          boolean indexExists = elasticSearchClient.admin().indices().prepareExists(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
+                  .execute().actionGet().isExists();
+          if (!indexExists) {
+            elasticSearchClient.admin().indices()
+                    .prepareCreate(ElasticSearchClient.SUBSONIC_MEDIA_INDEX_NAME)
+                    .addMapping("DIRECTORY",
+                            "path", "type=string,index=not_analyzed",
+                            "created", "type=date",
+                            "changed", "type=date",
+                            "childrenLastUpdated", "type=date",
+                            "lastPlayed", "type=date",
+                            "lastScanned", "type=date",
+                            "starredDate", "type=date")
+                    .execute().actionGet();
+          }
 
-                }
-            }
         }
-        return elasticSearchClient;
+      }
     }
+    return elasticSearchClient;
+  }
 
-    public ObjectMapper getMapper() {
-        return mapper;
-    }
+  public ObjectMapper getMapper() {
+    return mapper;
+  }
 }
--- subsonic-main/src/test/java/com/github/biconou/subsonic/service/MediaScannerServiceTestCase.java
+++ subsonic-main/src/test/java/com/github/biconou/subsonic/service/MediaScannerServiceTestCase.java
@@ -1,20 +1,50 @@
 package com.github.biconou.subsonic.service;
 
+import com.github.biconou.dao.ElasticSearchClient;
+import net.sourceforge.subsonic.domain.Player;
+import net.sourceforge.subsonic.service.*;
+import org.apache.commons.io.FileUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by remi on 01/05/2016.
  */
 public class MediaScannerServiceTestCase extends TestCase {
 
-  public void testScan () {
+  private static String baseResources = "/com/github/biconou/subsonic/service/mediaScannerServiceTestCase/";
 
-    String applicationContextService = "/com/github/biconou/subsonic/service/mediaScannerServiceTestCase/applicationContext-service.xml";
-    String applicationContextCache = "/com/github/biconou/subsonic/service/mediaScannerServiceTestCase/applicationContext-cache.xml";
+  private net.sourceforge.subsonic.service.MediaScannerService mediaScannerService = null;
 
-    String subsoncicHome = this.getClass().getResource("/com/github/biconou/subsonic/service/mediaScannerServiceTestCase/").toString().replace("file:/","");
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    // Prepare database
+    String baseDir = this.getClass().getResource(baseResources).toString().replace("file:/","");
+    String initDbDir = baseDir + "init_db";
+    String dbDir = baseDir + "db";
+    File dbDirectory = new File(dbDir);
+    if (dbDirectory.exists()) {
+      FileUtils.deleteDirectory(dbDirectory);
+    }
+    FileUtils.copyDirectory(new File(initDbDir),dbDirectory,true);
+
+    // delete logs
+    FileUtils.forceDelete(new File(baseDir + "subsonic.log"));
+    FileUtils.forceDelete(new File(baseDir + "subsonic.properties"));
+    FileUtils.forceDelete(new File(baseDir + "cmus.log"));
+    FileUtils.forceDelete(new File(baseDir + "mediaScanner.log"));
+
+    // load spring context
+    String applicationContextService = baseResources + "applicationContext-service.xml";
+    String applicationContextCache = baseResources + "applicationContext-cache.xml";
+
+    String subsoncicHome = this.getClass().getResource(baseResources).toString().replace("file:/","");
     System.setProperty("subsonic.home",subsoncicHome);
 
     String[] configLocations = new String[]{
@@ -23,12 +53,17 @@ public class MediaScannerServiceTestCase extends TestCase {
     };
     ApplicationContext context = new ClassPathXmlApplicationContext(configLocations);
 
-    net.sourceforge.subsonic.service.MediaScannerService mediaScannerService = (net.sourceforge.subsonic.service.MediaScannerService)context.getBean("mediaScannerService");
+    mediaScannerService = (net.sourceforge.subsonic.service.MediaScannerService)context.getBean("mediaScannerService");
+
+    // delete index
+    ElasticSearchClient ESClient = (ElasticSearchClient)context.getBean("elasticSearchClient");
+    ESClient.deleteIndex();
+
+  }
+
+  public void testScanLibrary () {
+
     mediaScannerService.scanLibrary();
-
-    //SettingsService settingsService = (SettingsService)context.getBean("settingsService");
-    //List<MusicFolder> liste = settingsService.getAllMusicFolders();
-
 
     while (mediaScannerService.isScanning()) {
       try {
@@ -38,6 +73,6 @@ public class MediaScannerServiceTestCase extends TestCase {
       }
     }
 
-    System.out.print("toto");
+    System.out.print("End");
   }
 }
