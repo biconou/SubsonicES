2bba04923a4a2b4d2cdac63eed336da1f9c48f77
biconou <rcocula@gmail.com>
2016-05-17 23:20:52 +0200

    Add unit test for performance

---
+++ subsonic-main/src/test/java/com/github/biconou/subsonic/service/PerformanceServiceTestCase.java
@@ -0,0 +1,113 @@
+package com.github.biconou.subsonic.service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import junit.framework.Assert;
+import net.sourceforge.subsonic.dao.AlbumDao;
+import net.sourceforge.subsonic.dao.MediaFileDao;
+import net.sourceforge.subsonic.domain.Album;
+import net.sourceforge.subsonic.domain.MediaFile;
+import net.sourceforge.subsonic.service.SearchService;
+import org.springframework.context.ApplicationContext;
+import com.github.biconou.subsonic.TestCaseUtils;
+import junit.framework.TestCase;
+import net.sourceforge.subsonic.dao.MusicFolderDao;
+
+/**
+ * Created by remi on 01/05/2016.
+ */
+public class PerformanceServiceTestCase extends TestCase {
+
+  private static String baseResources = "/com/github/biconou/subsonic/service/performanceServiceTestCase/";
+
+  private final MetricRegistry metrics = new MetricRegistry();
+  private ConsoleReporter reporter = null;
+  private JmxReporter jmxReporter = null;
+
+
+  private MediaFileDao mediaFileDao = null;
+  private AlbumDao albumDao = null;
+  private MusicFolderDao musicFolderDao = null;
+  private SearchService searchService = null;
+  private MediaScannerService mediaScannerService = null;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    reporter = ConsoleReporter.forRegistry(metrics)
+            .convertRatesTo(TimeUnit.SECONDS.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build();
+
+    jmxReporter = JmxReporter.forRegistry(metrics).build();
+    jmxReporter.start();
+
+
+    // Prepare database
+    TestCaseUtils.prepareDataBase(baseResources);
+
+    TestCaseUtils.setSubsonicHome(baseResources);
+
+    // load spring context
+    ApplicationContext context = TestCaseUtils.loadSpringApplicationContext(baseResources);
+
+    mediaFileDao = (MediaFileDao)context.getBean("mediaFileDao");
+    albumDao = (AlbumDao) context.getBean("albumDao");
+    musicFolderDao = (MusicFolderDao) context.getBean("musicFolderDao");
+    searchService = (SearchService) context.getBean("searchService");
+    mediaScannerService = (MediaScannerService)context.getBean("mediaScannerService");
+
+  }
+
+
+  public void testPerformaceAlbumBrowse() {
+
+
+    Timer scanTimer = metrics.timer(MetricRegistry.name(this.getClass(), "Timer.scan"));
+    Timer.Context globalTimerContext =  scanTimer.time();
+    TestCaseUtils.execScan(mediaScannerService);
+    globalTimerContext.stop();
+
+
+
+   /* try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } */
+
+
+    Timer loopTimer = metrics.timer(MetricRegistry.name(PerformanceServiceTestCase.class, "Timer.loop"));
+
+   // Timer.Context globalTimerContext =  globalTimer.time();
+
+    int i = 0;
+    while (i < 100) {
+      Timer.Context loopTimerContext = loopTimer.time();
+
+      // TODO mediaFolders ?
+      List<MediaFile> foundAlbums = searchService.getRandomAlbums(10,musicFolderDao.getAllMusicFolders());
+
+      foundAlbums.stream().forEach(album -> {
+        System.out.println("++++++ "+album.getAlbumName());
+        mediaFileDao.getSongsForAlbum(album.getArtist(),album.getAlbumName())
+                .stream().forEach(song -> System.out.println(song.getName()));
+      });
+
+
+      loopTimerContext.stop();
+      i++;
+    }
+
+    reporter.report();
+
+    System.out.print("End");
+  }
+
+}
